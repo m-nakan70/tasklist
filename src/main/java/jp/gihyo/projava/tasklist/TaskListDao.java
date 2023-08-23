2a24fc76e1ca2a4a8a974c40055a0a1decd2b63e
m-nakan70 <mio.nakanishi70@gmail.com>
2023-08-23 11:50:30 +0900

    タスク追加可、更新削除不可

--- src/main/java/jp/gihyo/projava/tasklist/TaskListDao.java
+++ src/main/java/jp/gihyo/projava/tasklist/TaskListDao.java
@@ -25,36 +25,34 @@ public class TaskListDao {
         SimpleJdbcInsert insert = new SimpleJdbcInsert(this.jdbcTemplate).withTableName(TABLE_NAME);
         return insert.execute(param);
     }
-    //ここまで更新
-    public List<TaskItem>findAll(){
+    public <LIST>List<HomeController.TaskItem>findAll(){
         String query = "SELECT * FROM " + TABLE_NAME;
-
-
-
-        List<Map<String, Object>> result = jdbcTemplate.queryForList(query);//SQL
-        List<TaskItem> taskItems = result.stream()
-                .map((Map<String, Object> row) -> new TaskItem(
+        List<Map<String, Object>> result = this.jdbcTemplate.queryForList(query);//SQL
+        List<HomeController.TaskItem> list = result.stream()
+                .map((Map<String, Object> row) -> new HomeController.TaskItem(
                         row.get("id").toString(),
                         row.get("task").toString(),
                         row.get("deadline").toString(),
                         row.get("memo").toString(),
                         (Boolean)row.get("done")))
                 .toList();
-        return taskItems;
+        return list;
     }
     public int delete(String id){
-        int number = jdbcTemplate.update("Delete FROM tasklist WHERE id = ?",id);
+        int number = jdbcTemplate.update("Delete FROM "+ TABLE_NAME + "WHERE id = ?", id);
         return number;
     }
-    public int update(TaskItem taskItem){
+    public int update(HomeController.TaskItem taskItem){
         int number = jdbcTemplate.update(
-                "UPDATE tasklist SET task = ?, deadline = ?, ,memo= ?,done = ? WHERE id = ?",
+                "UPDATE tasklist SET task = ?, deadline = ?,memo= ?,done = ? WHERE id = ?",
                 taskItem.task(),
                 taskItem.deadline(),
                 taskItem.deadline(),
+                taskItem.memo(),
                 taskItem.done(),
                 taskItem.id());
         return number;
 
     }
+    //ここまで更新
 }
